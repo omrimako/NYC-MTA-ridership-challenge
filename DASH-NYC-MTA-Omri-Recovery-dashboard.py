d09d20bf0c12e3297cdb5828b57f06a9cface0f2
Omri Makover <omri_makover@yahoo.com>
2024-12-17 10:54:01 -0500

    Update DASH-NYC-MTA-Omri-Recovery-dashboard.py

--- DASH-NYC-MTA-Omri-Recovery-dashboard.py
+++ DASH-NYC-MTA-Omri-Recovery-dashboard.py
@@ -8,6 +8,7 @@ import dash
 from dash import dcc, html, dash_table
 from dash.dependencies import Input, Output
 import pandas as pd
+import plotly.graph_objs as go
 
 # Prepare the data
 #df = pd.read_csv("/Users/omri_/Github_projects/Dask_competition_MTA/mta_data_ridership1.csv")
